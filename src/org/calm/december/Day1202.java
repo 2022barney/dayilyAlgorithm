9f0f5d28ca5dd74211d66fa275d07005f347e4df
liyang <973478637@qq.com>
2022-12-02 20:44:08 +0800

    1202

---
+++ src/org/calm/december/Day1202.java
@@ -0,0 +1,40 @@
+package org.calm.december;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * ------------------------
+ *
+ * ------------------------
+ * @author liyang
+ * @date 2022/12/2
+ * @version 1.0
+ * ------------------------
+ */
+public class Day1202 {
+    public static void main(String[] args) {
+        minOperations("001011");
+    }
+
+    public static int[] minOperations(String boxes) {
+
+        int n = boxes.length();
+        int[] ans = new int[n];
+        int step = 0, cnt = 0;
+        for (int i = 0; i < n; i++) {
+            step += cnt;
+            ans[i] += step;
+            if (boxes.charAt(i) == '1') cnt++;
+        }
+
+        step = 0; cnt = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            step += cnt;
+            ans[i] += step;
+            if (boxes.charAt(i) == '1') cnt++;
+        }
+        return ans;
+    }
+}
